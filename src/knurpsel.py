4a44f23ad33867ff61b1cc65999e5685a7fdf629
Max Zender <mail@maxzender.de>
2014-12-17 17:52:39 +0100

    add functionality for monitoring the reuqest count for each node

--- src/knurpsel.py
+++ src/knurpsel.py
@@ -9,20 +9,27 @@ class ServerNode ():
 	"""Represents a server node. Used to document the host,
 	   port and state of the server node."""
 	NODE_TIMEOUT = 5
+
 	def __init__ (self, host, port):
 		self.host = host
 		self.port = port
+		self.request_count = 0
 		self.activate ()
 		self.timer = None
+
 	def get_host (self):
 		return self.host
+
 	def get_port (self):
 		return self.port
+
 	def activate (self):
 		self.active = True
+
 	def timeout (self):
 		print '%s:%s timed out!' % (self.host, self.port)
 		self.active = False
+
 	def is_active (self):
 		return self.active
 
@@ -30,19 +37,23 @@ class NodeDatabase ():
 	"""Holds all nodes, which had been reporting, since the monitor
 	   service has been started. A node 'times out' after a given number
 	   of seconds, which means it is probably dead."""
+
 	def __init__ (self):
 		self.nodes = {}
 		self.timeout_timers = {}
+
 	def as_list (self):
 		node_list = []
 		for node_name, node in self.nodes.iteritems ():
 			node_list.append ({
 				'host': node.get_host (),
 				'port': node.get_port (),
-				'active': node.is_active ()
+				'active': node.is_active (),
+				'request_count': node.request_count
 			})
 		return node_list
-	def update (self, node_info):
+
+	def update_node (self, node_info):
 		host = node_info['host']
 		port = node_info['port']
 		key = '%s:%s' % (host, port)
@@ -51,9 +62,14 @@ class NodeDatabase ():
 		node = self.nodes[key]
 		node.activate ()
 		if key in self.timeout_timers:
-			self.timeout_timers[key].cancel ()		
+			self.timeout_timers[key].cancel ()
 		self.timeout_timers[key] = Timer (ServerNode.NODE_TIMEOUT, self.on_timeout, [key])
 		self.timeout_timers[key].start ()
+
+	def update_request_counts (self, request_counts):
+		for (key, request_count) in request_counts.items ():
+			self.nodes[key].request_count = request_count
+
 	def on_timeout (self, key):
 		self.nodes[key].timeout ()
 		del self.timeout_timers[key]
@@ -61,40 +77,62 @@ class NodeDatabase ():
 class BaseView ():
 	"""Base class for jinja templates. Should be used by controllers
 	   if they want to render stuff."""
+
 	def __init__ (self, path):
 		self.view_path = path
+
 	def render (self, *data, **data_map):
 		return render_template (self.view_path, **data_map)
 
 class ReportController ():
 	"""Controller responsible for doing all task related to reporting."""
+
 	def __init__ (self):
 		self.nodes = NodeDatabase ()
 		self.view = BaseView ('report.html')
+
 	def index_get_json (self, request):
 		return Response (json.dumps (self.nodes.as_list ()), status=200)
+
+	def update_request_counts_post (self, request):
+		ct = request.headers['Content-Type']
+
+		if ct == 'application/json':
+			nodes = request.json
+			self.nodes.update_request_counts (nodes)
+
+			r = Response ("thanks for reporting your status!", status=200)
+		else:
+			r = Response ("unexpected Content-Type: " + ct)
+
+		return r
+
 	def index_get (self, request):
 		view_str = self.view.render (nodes=self.nodes.as_list ())
 		return Response (view_str, status=200)
+
 	def index_post (self, request):
 		r = None
 		ct = request.headers['Content-Type']
 
 		if ct == 'application/json':
 			json_obj = request.json
-			self.nodes.update (json_obj)
+			self.nodes.update_node (json_obj)
 			r = Response ("thanks for reporting your status!", status=200)
 		else:
 			r = Response ("unexpected Content-Type: " + ct)
 		return r
+
 	def default (self, request):
 		return Response ('unexpected request!', status=404)
 
 class IndexController ():
 	"""Controller responsible for handling all index actions"""
+
 	def __init__ (self):
 		self.link_to_report = '/report'
 		self.view = BaseView ('index.html')
+
 	def index_get (self, request):
 		return self.view.render (link=self.link_to_report)
 
@@ -113,8 +151,7 @@ def respond_index ():
 @app.route ('/report.json', methods=['GET'])
 def respond_report_json ():
 	c = app.controllers['report']
-	r = c.index_get_json (request)
-	return r
+	return c.index_get_json (request)
 
 @app.route ("/report", methods=['GET', 'POST'])
 def respond_report ():
@@ -127,6 +164,12 @@ def respond_report ():
 	else:
 		r = c.default (request)
 	return r
+
+@app.route ("/update_request_counts", methods=['POST'])
+def respond_update_request_counts ():
+	print "called"
+	c = app.controllers['report']
+	return c.update_request_counts_post (request)
 
 if __name__ == "__main__":
 	app.run (host='0.0.0.0', port=4242, debug=True)
